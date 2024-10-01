c8c9a352424e939a1025e82b621e1b6375f57022
Rodrigo Sul <127897775+rodrigosul1@users.noreply.github.com>
2024-10-01 09:16:46 -0300

    Update CORS

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from fastapi import FastAPI, HTTPException, Query
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api._errors import NoTranscriptFound, VideoUnavailable
 
 app = FastAPI()
 
+# Adicionar middleware CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permitir todos os domínios (você pode restringir conforme necessário)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 def extract_video_id(video_url: str) -> str:
-    # Extrair o ID do vídeo da URL do YouTube
     if "v=" in video_url:
         return video_url.split("v=")[1].split("&")[0]
     elif "youtu.be/" in video_url:
@@ -15,20 +24,17 @@ def extract_video_id(video_url: str) -> str:
 
 def generate_transcript(video_id: str):
     try:
-        # Obter a transcrição em inglês e português, se disponíveis
         transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['en', 'pt'])
-        # Juntar todas as falas em um texto único
         full_text = " ".join([t['text'] for t in transcript])
         return full_text
     except (NoTranscriptFound, VideoUnavailable) as e:
-        raise HTTPException(status_code=404, detail=str(e))
+        return {"error": str(e)}
 
 @app.get("/get-transcript/")
-def get_transcript(url: str = Query(..., description="URL do vídeo do YouTube")):
+def get_transcript(url: str):
     try:
         video_id = extract_video_id(url)
         transcript = generate_transcript(video_id)
         return {"transcript": transcript}
     except ValueError as e:
-        raise HTTPException(status_code=400, detail=str(e))
-
+        return {"error": str(e)}
